3510f561691f500c5df1af6f762587a1162f90e0
PokyClover <jlp0067@uah.edu>
2025-11-26 13:27:08 -0600

    Added exposition

---
+++ Zombie Game/Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+
+  public void OnStartPress()
+    {
+        SceneManager.LoadScene("Scene_0");
+    }
+
+    public void OnNextPress()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
